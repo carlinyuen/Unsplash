2a62d963b4691c6bf5f954612ad7912151cd7ed5
Carlin <email.me@carlinyuen.com>
2014-02-26 18:13:22 -0500

    adding parallax

--- iOS/Unsplash/ViewControllers/USImageViewController.m
+++ iOS/Unsplash/ViewControllers/USImageViewController.m
@@ -44,6 +44,31 @@
 	tap.delaysTouchesBegan = false;
 	tap.delaysTouchesEnded = false;
     [self.scrollView addGestureRecognizer:tap];
+
+    // Adding parallax effect for iOS 7
+    if (false)  // TODO: check for iOS 7
+    {
+        // Set vertical effect
+        UIInterpolatingMotionEffect *verticalMotionEffect
+            = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.y"
+            type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+        verticalMotionEffect.minimumRelativeValue = @(-10);
+        verticalMotionEffect.maximumRelativeValue = @(10);
+        
+        // Set horizontal effect 
+        UIInterpolatingMotionEffect *horizontalMotionEffect
+            = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x"
+            type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        horizontalMotionEffect.minimumRelativeValue = @(-10);
+        horizontalMotionEffect.maximumRelativeValue = @(10);
+        
+        // Create group to combine both
+        UIMotionEffectGroup *group = [UIMotionEffectGroup new];
+        group.motionEffects = @[horizontalMotionEffect, verticalMotionEffect];
+        
+        // Add both effects to your view
+        [self.scrollView addMotionEffect:group];
+    }
 }
 
 - (void)didReceiveMemoryWarning
